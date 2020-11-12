150e10d774245808d66e89f5ac93aa21b59484c0
Markus Feng <markusfeng@gmail.com>
2020-11-12 03:16:27 -0500

    Added more tests for shell/subshell/escaping

--- test/dresh_test.ml
+++ test/dresh_test.ml
@@ -4,16 +4,52 @@ open Dresh_shell
 
 let run_test input =
   let stdout = force Writer.stdout in
-  Ast.parse input
-  |> Or_error.map ~f:(fun ast -> eval ast ~writer:stdout ~to_close:false ~verbose:false)
-;;
-
-let%expect_test "test echo" =
-  match run_test "echo \"hello\"" with
+  match Ast.parse input with
   | Error err ->
     print_endline (Error.to_string_hum err);
     return ()
-  | Ok res ->
-    let%bind () = res in
-    [%expect {|hello|}]
+  | Ok ast -> eval ast ~writer:stdout ~to_close:false ~verbose:false
+;;
+
+let%expect_test "echo" =
+  let%bind () = run_test "echo hello" in
+  [%expect {|hello|}]
+;;
+
+let%expect_test "empty" =
+  let%bind () = run_test "" in
+  [%expect {||}]
+;;
+
+let%expect_test "quotes" =
+  let%bind () = run_test "echo \"hello\"" in
+  [%expect {|hello|}]
+;;
+
+let%expect_test "concat" =
+  let%bind () = run_test "echo \"hello\"world\"!\"" in
+  [%expect {|helloworld!|}]
+;;
+
+let%expect_test "subshell_simple" =
+  let%bind () = run_test "echo $(echo \"abcdef\")" in
+  [%expect {|abcdef|}]
+;;
+
+let%expect_test "subshell_complex" =
+  let%bind () = run_test "echo \"ab$(echo \"cd\")e\"fg" in
+  [%expect {|abcdefg|}]
+;;
+
+let%expect_test "subshell_name" =
+  let%bind () = run_test "\"e\"$(echo \"ch\")o \"a$(echo \"b\")c\"d" in
+  [%expect {|abcd|}]
+;;
+
+let%expect_test "escape" =
+  let%bind () =
+    run_test
+      "\\ec\\ho \\\"\\\"\"\\$(echo fake\\ subshell)$(echo \"real subshell\"\\)\\\\)\""
+  in
+  [%expect {|""$(echo fake subshell)real subshell)\|}]
 ;;
