725d8e308cd6c1551ccfe4329c2ad9b4b94f86a4
Markus Feng <markusfeng@gmail.com>
2021-05-04 18:59:02 -0400

    Temporary update

--- examples/line_length_ssh.sh
+++ examples/line_length_ssh.sh
@@ -5,14 +5,19 @@ m2=machine2.cs.williams.edu
 m3=machine3.cs.williams.edu
 m4=machine4.cs.williams.edu
 
+# Cleanup from prior runs
+rm map_*.txt
+rm all_keys.txt
+rm unique_keys.txt
+
 # Map operation
 # Loop through each line of the input
 while IFS= read -r line; do
     # Choose a random host
     # Note that the POSIX shell does not support arrays
-    target=$(echo -e "$m1\n$m2\n$m3\n$m4" | shuf)
+    target=$(echo -e "$m1\n$m2\n$m3\n$m4" | shuf -n 1)
     # Execute map job on remote host
-    result=$(ssh "$target" "echo '$line' | wc -c; echo 1")
+    result=$(echo "" | ssh "$target" "(echo \"$line\" | wc -c; echo 1)")
     # Retreive the key
     key=$(echo "$result" | head -n 1)
     # Retreive the value
@@ -26,18 +31,18 @@ while IFS= read -r line; do
 done < input.txt
 
 # Deduplicate key list
-sort all_keys.txt | uniq -u > unique_keys.txt
+sort all_keys.txt | uniq > unique_keys.txt
 
 # Reduce operation
 # Loop through each key
 while IFS= read -r line; do
     # Choose a random host
     # Note that the POSIX shell does not support arrays
-    target=$(echo -e "$m1\n$m2\n$m3\n$m4" | shuf)
+    target=$(echo -e "$m1\n$m2\n$m3\n$m4" | shuf -n 1)
     # Generate the input file depending on the key
     infile="map_$line.txt"
     # Execute reduce job on remote host
-    result=$(cat "$infile" | ssh "$target" "paste -sd+ - | bc")
+    result=$(cat "$infile" | ssh "$target" "(paste -sd+ - | bc)")
     # Print result to the standard output
     echo "$line,$result"
 done < unique_keys.txt
